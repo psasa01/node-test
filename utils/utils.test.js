fc59c5968c1f24c8f2dbc44cb26a6013bf83568d
psasa01 <psasa01@gmail.com>
2017-10-18 17:00:38 +0200

    181017

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,5 +1,34 @@
 const utils = require('./utils');
+const expect = require('expect');
 
 it('should add 2 numbers', () => {
     var res = utils.add(33, 11);
+
+    expect(2).toBeA('number');
+    expect(res)
+        .toBe(44);
+
 });
+
+it('should square a number', () => {
+    var res = utils.square(3);
+
+    expect(res).toBe(9).toBeA('number');
+});
+
+it('should verify first and last names are set', () => {
+    var user = {
+        location: "Sarajevo",
+        age: 38
+    };
+
+    var res = utils.setName(user, "Saša Petrović");
+
+    utils.setName("Sale", "Saša Petrović");
+
+    expect(user).toInclude({
+        firstName: 'Saša',
+        lastName: 'Petrović'
+    });
+
+})
